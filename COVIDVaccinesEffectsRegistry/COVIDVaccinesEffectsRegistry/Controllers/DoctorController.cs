a3f833c7330fbbc48d2f0e4bc2704b25344b5734
Francisco Mata <francisco.redondo23@gamil.com>
2022-07-18 22:08:24 -0600

    front and controller done

---
+++ COVIDVaccinesEffectsRegistry/COVIDVaccinesEffectsRegistry/Controllers/DoctorController.cs
@@ -0,0 +1,42 @@
+﻿using COVIDVaccinesEffectsRegistry.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COVIDVaccinesEffectsRegistry.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private readonly COVIDVaccinesEffectsContext _context;
+
+        public DoctorController(COVIDVaccinesEffectsContext context) => _context = context;
+
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAsync(DoctorViewModel doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                Doctor doctorEF = new Doctor()
+                {
+                    DocumentId = doctor.DocumentId,
+                    ProfessionalCode = doctor.ProfessionalCode,
+                    Name = doctor.Name,
+                    Email = doctor.Email,
+                    Country = doctor.Country,
+                    Province = doctor.Province
+                };
+
+                _context.Add(doctorEF);
+                await _context.SaveChangesAsync();
+                //return RedirectToAction(nameof(Index));
+            }
+
+            return View(doctor);
+        }
+    }
+}
