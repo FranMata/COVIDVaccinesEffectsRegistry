6c5007cfc65cb75e97ec56cce03c2b6c62acfe0f
Francisco Mata <francisco.redondo23@gamil.com>
2022-08-05 12:32:24 -0600

    4 done

---
+++ COVIDVaccinesEffectsRegistry/COVIDVaccinesEffectsRegistry/Controllers/PatientController.cs
@@ -0,0 +1,49 @@
+﻿using COVIDVaccinesEffectsRegistry.Models.OtherModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COVIDVaccinesEffectsRegistry.Models.ViewModels
+{
+    public class PatientController : Controller
+    {
+        private COVIDVaccinesEffectsContext _context;
+
+        public PatientController(COVIDVaccinesEffectsContext context) => _context = context;
+        public ActionResult Index() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(PatientViewModel patient)
+        {
+            if (ModelState.IsValid)
+            {
+                Patient patientEF = new Patient()
+                {
+                    DocumentId = patient.DocumentId,
+                    Name = patient.Name,
+                    LastName = patient.LastName,
+                    BirthDate = patient.BirthDate.ToString(),
+                    Gender = 1,
+                    ContactNumber = patient.ContactNumber,
+                    Country = patient.Country,
+                    Province = patient.Province,
+                    District = patient.District,
+                    MaritalStatus = patient.MaritalStatus,
+                    Email = patient.Email,
+                    RegistryDate = patient.RegistryDate.ToString(),
+                    Occupation = patient.Occupation,
+                    DoctorId = DoctorClinicData.DoctorId,
+                    ClinicId = DoctorClinicData.ClinicId
+                };
+                _context.Add(patientEF);
+                await _context.SaveChangesAsync();
+                //return Redirect(@"/Clinic");
+            }
+            return View(patient);
+        }
+    }         
+}
---
+++ COVIDVaccinesEffectsRegistry/COVIDVaccinesEffectsRegistry/Models/ViewModels/PatientViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COVIDVaccinesEffectsRegistry.Models.ViewModels
+{
+    public class PatientViewModel
+    {
+        [Required]
+        [Display(Name = "Cedula")]
+        public int? DocumentId { get; set; }
+        [Required]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Apellido")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Fecha de nacimiento")]
+        public DateTime BirthDate { get; set; }
+        [Required]
+        [Display(Name = "Sexo")]
+        public string? Gender { get; set; }
+        [Required]
+        [Display(Name = "Numero de contacto")]
+        public int? ContactNumber { get; set; }
+        [Required]
+        [Display(Name = "Pais")]
+        public string Country { get; set; }
+        [Required]
+        [Display(Name = "Provincia")]
+        public string Province { get; set; }
+        [Required]
+        [Display(Name = "Distrito")]
+        public string District { get; set; }
+        [Required]
+        [Display(Name = "Estado civil")]
+        public int? MaritalStatus { get; set; }
+        [Required]
+        [Display(Name = "Correo electronico")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Fecha de registro")]
+        public DateTime RegistryDate { get; set; }
+        [Required]
+        [Display(Name = "Ocupacion")]
+        public string Occupation { get; set; }        
+        public int? DoctorId { get; set; }        
+        public int? ClinicId { get; set; }
+    }
+}
