c8be3b0bb64ce279b1b72425d95103cfc70c9fc8
Francisco Mata <francisco.redondo23@gamil.com>
2022-08-05 14:19:03 -0600

    5b done

---
+++ COVIDVaccinesEffectsRegistry/COVIDVaccinesEffectsRegistry/Controllers/PatientMedicalHistoryController.cs
@@ -0,0 +1,49 @@
+﻿using COVIDVaccinesEffectsRegistry.Models;
+using COVIDVaccinesEffectsRegistry.Models.OtherModels;
+using COVIDVaccinesEffectsRegistry.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
+
+namespace COVIDVaccinesEffectsRegistry.Controllers
+{
+    public class PatientMedicalHistoryController : Controller
+    {
+        private readonly COVIDVaccinesEffectsContext _context;
+
+        public PatientMedicalHistoryController(COVIDVaccinesEffectsContext context) => _context = context;
+
+        public IActionResult Index()
+        {
+            ViewData["Answers"] = new SelectList(_context.Answers, "Id", "Aswer");
+            return View();
+        }            
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PatientMedicalHistoryViewModel patientMedicalHistory) //Guarda los datos del medico
+        {
+            if (ModelState.IsValid)
+            {
+                PatientMedicalHistory patientMedicalHistoryEF = new PatientMedicalHistory()
+                {
+                    HadCovidprevios = patientMedicalHistory.HadCovidprevios,
+                    //HadSuspicionCov = patientMedicalHistory.HadSuspicionCovid,
+                    HadCovidafter = patientMedicalHistory.HadCovidafter,
+                    Reason = patientMedicalHistory.Reason,
+                    IsPregnant = patientMedicalHistory.IsPregnant,
+                    HadVaccineReaction = patientMedicalHistory.HadVaccineReaction,
+                    VaccineReactionDescription = patientMedicalHistory.VaccineReactionDescription,
+                    ActualMedicine = patientMedicalHistory.ActualMedicine,
+                    MedicineAfterCovidveccine = patientMedicalHistory.MedicineAfterCovidveccine,
+                    PatientId = DataToKeep.PatientId
+
+                };
+                _context.Add(patientMedicalHistoryEF);
+                await _context.SaveChangesAsync();
+                return Redirect(@"/Clinic");
+            }
+            return View(patientMedicalHistory);
+        }
+    }
+}
---
+++ COVIDVaccinesEffectsRegistry/COVIDVaccinesEffectsRegistry/Models/ViewModels/PatientMedicalHistoryViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COVIDVaccinesEffectsRegistry.Models.ViewModels
+{
+    public class PatientMedicalHistoryViewModel
+    {
+        [Required]
+        [Display(Name = "¿Ha tenido COVID previo a inyectarse?")]
+        public int HadCovidprevios { get; set; }
+        [Required]
+        [Display(Name = "¿Ha tenido sospecha de haber tenido COVID antes de ponerte lainyección ?")]
+        public int HadSuspicionCovid { get; set; }
+        [Required]
+        [Display(Name = "¿Ha tenido COVID después de tomar la inyección?")]
+        public int HadCovidafter { get; set; }
+        [Required]
+        [Display(Name = "¿Razón de inyectarse contra COVID?")]
+        public string Reason { get; set; }
+        [Required]
+        [Display(Name = "¿Estaba embarazada al inyectarse contra COVID? Si aplica")]
+        public int IsPregnant { get; set; }
+        [Required]
+        [Display(Name = "¿Ha tenido reacciones a vacunas en el pasado?")]
+        public int HadVaccineReaction { get; set; }
+        [Required]
+        [Display(Name = "Especifique las reacciones a vacunas en el pasado")]
+        public string VaccineReactionDescription { get; set; }
+        [Required]
+        [Display(Name = "Medicamentos actuales con receta: enumere todos los medicamentos y las dosis")]
+        public string ActualMedicine { get; set; }
+        [Required]
+        [Display(Name = "Nuevos medicamentos recetados que tuvieron que ser iniciados después de la(s) inyección(es) de COVID")]
+        public string MedicineAfterCovidveccine { get; set; }
+    }
+}
