5fbbeee1d58caed1bcfba21865f65eaf5846b99e
Francisco Mata <francisco.redondo23@gamil.com>
2022-08-08 15:39:57 -0600

    add tables

---
+++ COVIDVaccinesEffectsRegistry/COVIDVaccinesEffectsRegistry/Helper/AdverseEffectsAndDataHelper.cs
@@ -0,0 +1,112 @@
+﻿using COVIDVaccinesEffectsRegistry.Models;
+using COVIDVaccinesEffectsRegistry.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COVIDVaccinesEffectsRegistry.Helper
+{
+    public static class AdverseEffectsAndDataHelper
+    {
+        public static List<AllergyViewModel> BuildAllergyViewModel(List<Allergy> allergies)
+        {
+            List<AllergyViewModel> allergiesViewModels = new List<AllergyViewModel>();
+
+            allergies.ForEach(e =>
+            {
+                allergiesViewModels.Add(new AllergyViewModel()
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    IsChecked = false
+                });
+            });            
+            return allergiesViewModels;
+        }
+
+        public static List<AllergyRegistered> BuildAllergy(List<AllergyViewModel> allergyViewModels, int patientId)
+        {
+            List<AllergyRegistered> allergies = new List<AllergyRegistered>();
+
+            List<AllergyViewModel> allergiesChecked = allergyViewModels.Where(e => e.IsChecked).ToList();
+
+            allergiesChecked.ForEach(e =>
+            {
+                allergies.Add(new AllergyRegistered()
+                {
+                    AllergyId = e.Id,
+                    PatientId = patientId
+                });
+            });
+
+            return allergies;
+        }
+
+        public static List<DeseaseViewModel> BuildDeseaseViewModel(List<Disease> deseases)
+        {
+            List<DeseaseViewModel> deseasesViewModels = new List<DeseaseViewModel>();
+
+            deseases.ForEach(e =>
+            {
+                deseasesViewModels.Add(new DeseaseViewModel()
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    IsChecked = false
+                });
+            });
+            return deseasesViewModels;
+        }
+
+        public static List<DiseaseRegistered> BuildDesease(List<DeseaseViewModel> deseaseViewModels, int patientId)
+        {
+            List<DiseaseRegistered> deseases = new List<DiseaseRegistered>();
+
+            List<DeseaseViewModel> deseaseChecked = deseaseViewModels.Where(e => e.IsChecked).ToList();
+
+            deseaseChecked.ForEach(e =>
+            {
+                deseases.Add(new DiseaseRegistered()
+                {
+                    DeseaseId = e.Id,
+                    PatientId = patientId
+                });
+            });
+            return deseases;
+        }
+
+        public static List<SymptomViewModel> BuildSymptomViewModel(List<Symptom> symptom)
+        {
+            List<SymptomViewModel> symptomViewModels = new List<SymptomViewModel>();
+
+            symptom.ForEach(e =>
+            {
+                symptomViewModels.Add(new SymptomViewModel()
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    IsChecked = false
+                });
+            });
+            return symptomViewModels;
+        }
+
+        public static List<SymptomRegistered> BuildSymptom(List<SymptomViewModel> symptomViewModels, int patientId)
+        {
+            List<SymptomRegistered> symptoms = new List<SymptomRegistered>();
+
+            List<SymptomViewModel> symptomChecked = symptomViewModels.Where(e => e.IsChecked).ToList();
+
+            symptomChecked.ForEach(e =>
+            {
+                symptoms.Add(new SymptomRegistered()
+                {
+                    SymptomId = e.Id,
+                    PatientId = patientId
+                });
+            });
+            return symptoms;
+        }
+    }   
+}
---
+++ COVIDVaccinesEffectsRegistry/COVIDVaccinesEffectsRegistry/Models/ViewModels/AdverseEffectsAndDataViewModel.cs
@@ -0,0 +1,27 @@
+﻿using COVIDVaccinesEffectsRegistry.Helper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COVIDVaccinesEffectsRegistry.Models.ViewModels
+{
+    public class AdverseEffectsAndDataViewModel
+    {
+        [Display(Name = "¿Se mantiene los síntomas?")]
+        public int KeepSymptom { get; set; }
+
+        [Display(Name = "Descripción de alergias conocidas")]
+        public string AllergyDescription { get; set; }
+
+        [Display(Name = "Especifique otras condiciones")]
+        public string OtherConditions { get; set; }
+
+        [Display(Name = "¿Si ha desarrollado un nuevo cáncer o la reaparición de un cáncer existente después de la inyección de COVID, especifique el tipo de cáncer ?")]
+        public string TypeNewCancer { get; set; }
+        public List<AllergyViewModel> allergyViewModels { get; set; }
+        public List<DeseaseViewModel> deseaseViewModel { get; set; }
+        public List<SymptomViewModel> symptomViewModel { get; set; }
+    }    
+}
